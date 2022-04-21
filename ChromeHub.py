00870b2853ffde09dab36d80bd41540050f35bdd
muffin-man300 <theghostphish.ofdoom37@gmail.com>
2022-04-21 21:20:56 +0530

    Version 1.1 - Added ability to connect to an IP Address. More cooler changes coming soon.

--- ChromeHub.py
+++ ChromeHub.py
@@ -19,7 +19,12 @@ def open_google():
 def open_gmail():
     driver = webdriver.Chrome(service=s)
     driver.get('https://mail.google.com/')
+    
 
+def ip():
+    driver = webdriver.Chrome(service=s)
+    ip_in = ip_input.get()
+    driver.get("https://" + ip_in)
 
 open_website_head = tk.Label(root, text="Open Website")
 open_website_head.grid(row=0, column=0)
@@ -27,4 +32,10 @@ open_web_btn_1 = tk.Button(root, text="Google", command=lambda: [open_google()])
 open_web_btn_1.grid(row=1, column=0)
 open_web_btn_2 = tk.Button(root, text="Gmail", command=lambda: [open_gmail()])
 open_web_btn_2.grid(row=1, column=1)
+connect_ip_head = tk.Label(root, text="Connect IP Address")
+connect_ip_head.grid(row=2, column=0)
+ip_input = tk.Entry(root, width=30)
+ip_input.grid(row=3, column=0)
+ip_connect = tk.Button(root, text="Connect", command=lambda: [ip()])
+ip_connect.grid(row=3, column=1)
 root.mainloop()
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 **This is a very basic software, but im hoping to make it better over a long period of time**
 
+[Visit the repo!](https://github.com/muffin-man300/ChromeHub.git)
